a0d0ba2bce48b45fe1d0db9b9dd3a04a19ec2318
almogalili <41727312+almogalili@users.noreply.github.com>
2019-09-08 14:16:23 +0300

    Add files via upload

---
+++ instabot.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+import time
+
+
+class InstagramBot:
+
+    def __init__(self):
+        self.images = []
+        self.username = '_JuanKantor_'
+        self.password = '12i12144'
+        self.base_url = 'https://www.instagram.com'
+        self.driver = webdriver.Chrome('chromedriver.exe')
+        self.posts_urls = []
+        self.login()
+
+    def login(self):
+        login_page = '{}/accounts/login/'.format(self.base_url)
+        username_xpath = '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input'
+        password_xpath = '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input'
+        logging_button_xpath = '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button/div'
+
+        self.driver.get(login_page)
+        time.sleep(1)
+
+        self.driver.find_element_by_xpath(username_xpath).send_keys(self.username)
+        time.sleep(1)
+
+        self.driver.find_element_by_xpath(password_xpath).send_keys(self.password)
+        time.sleep(1)
+
+        self.driver.find_element_by_xpath(logging_button_xpath).click()
+        time.sleep(3)
+
+    def nav_user(self, user):
+        self.driver.get('{}/{}'.format(self.base_url, user))
+
+    # we need to scroll down and to save the images urls into an array
+    def scroll_down(self):
+
+        posts_xpath = "//*[@class='v1Nh3 kIKUG  _bz0w']//a"
+        last_height = self.driver.execute_script('return document.body.scrollHeight')
+        time.sleep(3)
+
+        while True:
+            # now we have to store the posts urls.
+            self.driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            path = self.driver.find_elements_by_xpath(posts_xpath)
+            time.sleep(1)
+            self.add_posts_urls(path)
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+
+            # this condition checking if we have to scroll down more.
+            if last_height == new_height:
+                return
+
+            last_height = new_height
+
+    def add_posts_urls(self, path):
+
+        for i in path:
+            url = i.get_attribute('href')
+            if url not in self.posts_urls:
+                self.posts_urls.append(url)
+
+    def get_images_from_posts(self):
+
+        for url in self.posts_urls:
+            self.driver.execute_script("window.open('" + url + "', '_self')")
+            self.driver.implicitly_wait(2)
+            imagesXpath = self.driver.find_elements_by_xpath("//*[@class='FFVAD']")
+
+            for x in imagesXpath:
+                img = x.get_attribute("srcset")
+                # in srcset there's about 3-4 resolution images url seperated by ,
+                img = img.split(",")
+                img = img[-1][:-6]
+                print(img)
+                if img not in self.images and img is not None:
+                    # last one being highest res image -4 to escape resolution X*X in url
+                    self.images.append(img)
+
+    def get_images(self, username):
+        self.nav_user(username)
+        self.scroll_down()
+        self.get_images_from_posts()
+
+        return self.images
---
+++ teleinstabot.py
@@ -0,0 +1,66 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from instabot import InstagramBot
+
+
+def start(update, context):
+    keyboard = [[InlineKeyboardButton("Download instagram images.", callback_data="1")],
+                [InlineKeyboardButton("Option 2", callback_data='2')],
+
+                [InlineKeyboardButton("Option 3", callback_data='3')],
+                [InlineKeyboardButton('Option 4', callback_data='4')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+
+def button(update, context):
+    query = update.callback_query
+
+    if query.data == '1':
+        query.edit_message_text(text="Please enter user name for download his images "
+                                     "by the syntax : /downloadimages username.\n"
+                                     "Note: if the username is private,we cannot download his images.")
+
+
+def download_images(update, context):
+    # if the username of instagram is starting with __ (double underscore) and end with double underscore
+    # the bot take without the underscores, so i have to figure out how to ignore that.
+    chat_id = update.message.chat_id
+    username = context.args
+
+    if len(username) == 0:
+        context.bot.send_message(chat_id=chat_id, text="You must enter at least one username.")
+        return
+
+    elif len(username) == 1:
+        context.bot.send_message(chat_id=chat_id,
+                                 text=f"Starting download images from: {username[0]} \n The process will take a few minutes. ")
+        ig_bot = InstagramBot()
+        images_urls = ig_bot.get_images(username[0])
+
+        if len(images_urls) == 0:
+            context.bot.send_message(chat_id=chat_id, text="My friend, check if the account is private.\n"
+                                                           "Or the account has no images.")
+        else:
+            for image in images_urls:
+                context.bot.send_photo(chat_id=chat_id, photo=image)
+
+    elif username[1] is not None:
+        update.message.reply_text("You cannot pass more than one username.")
+        return
+
+
+def main():
+    updater = Updater('985887921:AAGdkxD_ne5MpQ521NZY8BknqKcIQvwrJ6A', use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('downloadimages', download_images))
+    dp.add_handler(CallbackQueryHandler(button))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
